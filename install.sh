0d20214be407299e60346c53a8d4ebccf52b00e5
Louis Vera <imnotatwork@gmail.com>
2012-04-06 10:28:07 -0400

    more concise syntax

--- install.sh
+++ install.sh
@@ -3,16 +3,13 @@
 for name in '.bashrc' '.gitconfig' '.emacs'
 do
   file="${HOME}/$name"
-  if [ -h "$file" ]
-  then
+  if [ -h "$file" ]; then
     echo "removing symlink for $file"
     rm "$file"
-  elif [ -f "$file" ]
-  then
+  elif [ -f "$file" ]; then
     echo "moving $file to $file.bak"
     mv "$file" "$file.bak"
-  elif [ -e "$file" ]
-  then
+  elif [ -e "$file" ]; then
     echo "$file is not a file or a symlink"
   fi
   ln -s "${PWD}/$name" "$file"
